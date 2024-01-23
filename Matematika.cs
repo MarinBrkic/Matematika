7cfb2a5fbb8297bb812fc11ed44183c7b04438b1
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-23 11:49:44 +0100

    Add files via upload

---
+++ Matematika.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematika
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3, b = 4, c = 7, d = 2;
+            double x = ((a * a) + (b * b) + (c * c) + (d * d)) / (((a + b) * (a + b)) + ((c + d) * (c + d)));
+            Console.WriteLine("Rezultat: " + x);
+
+            Console.ReadLine();
+        }
+    }
+}
